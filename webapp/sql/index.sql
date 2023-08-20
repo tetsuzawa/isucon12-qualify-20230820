a9b4ae8219e1d370c3ce07954cbc4f8a0a71b1b3
Tetsu Takizawa <tetsu.takizawa5@gmail.com>
2023-08-20 18:39:31 +0900

    dup

--- webapp/sql/index.sql
+++ webapp/sql/index.sql
@@ -1,9 +1,6 @@
 CREATE INDEX competition_tenant_id_created_at_index
     ON isuports.competition (tenant_id, created_at ASC);
 
-CREATE INDEX competition_tenant_id_created_at_index
-    ON isuports.competition (tenant_id, created_at ASC);
-
 CREATE UNIQUE INDEX visit_history_idx ON visit_history (tenant_id, player_id, competition_id);
 
 CREATE INDEX visit_history_tenant_id_competition_id_pid_created_at_index
@@ -11,9 +8,6 @@ CREATE INDEX visit_history_tenant_id_competition_id_pid_created_at_index
 
 CREATE INDEX player_score_tenant_id_competition_id_player_id_index
     ON isuports.player_score (tenant_id, competition_id, player_id);
-
-CREATE INDEX player_tenant_id_created_at
-    ON isuports.player (tenant_id, created_at);
 
 CREATE INDEX player_tenant_id_created_at
     ON isuports.player (tenant_id, created_at);
